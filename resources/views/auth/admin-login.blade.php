02ee7eaba98cf878ffb48aed87a5193755a2c6d4
M Rizki Arya Nanda <rizkiarya0611@gmail.com>
2026-03-11 21:56:03 +0700

    Refactor code structure for improved readability and maintainability

--- resources/views/auth/admin-login.blade.php
+++ resources/views/auth/admin-login.blade.php
@@ -4,10 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <title>Login Admin</title>
 </head>
 <body>
-    <h2 class="text-blue text-2xl">Login Admin</h2>
+    <h2 class="text-blue-400 text-2xl">Login Admin</h2>
     @if(session('error'))
     <script>
         alert("{{ session('error') }}");
